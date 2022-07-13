e71bd832c78f08321c16ddb3298e45e5ac71aad4
maga195 <gucci_195@mail.ru>
2022-07-13 22:36:12 +0300

    added changing this to feat/cart-logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-// eslint-disable-next-line
-import { useState } from 'react';
 
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 
-function App() {
-  const [cartIsShown, setCartIsShown] = useState(false);
-
-  function showCartHandler () {
-    setCartIsShown(true);
-  }
-
-  function hideCartHandler () {
-    setCartIsShown(false);
-  }
 
   return (
     <CartProvider>
-      {cartIsShown && <div>Cart...</div>}
-      <Header onShowCart={showCartHandler} />
+      <Header/>
       <main>
         <Meals />
       </main>
@@ -28,4 +15,3 @@ function App() {
 }
 
 export default App;
-// eslint-disable-next-line
